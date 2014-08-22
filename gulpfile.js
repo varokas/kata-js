d01697229b1cd3a9798065c11a3f75326817323b
Varokas Panusuwan <varokas@panusuwan.com>
2014-08-22 00:00:48 +0000

    change test to use jasmine-node

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,30 @@
 var gulp = require('gulp');
+var karma = require('karma').server;
+var jasmine = require('gulp-jasmine');
+
+var path = {
+  specs: "specs/js/*spec.js"
+};
+
 
 gulp.task('default', function(){
 
+});
+
+var karmaConf = {
+  browsers: ['PhantomJS'],
+  frameworks: ['jasmine'],
+  files: [
+    path.specs,
+  ],
+  singleRun: true
+};
+
+gulp.task('test', function(done) {
+  gulp.src(path.specs)
+        .pipe(jasmine());
+});
+
+gulp.task('karma', function(done) {
+  karma.start(karmaConf, done);
 });
